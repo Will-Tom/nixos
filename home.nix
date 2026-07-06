4b7719c402eb0c8d40f8629c4b5bd18f68749683
Will Thompson <will.lachlan.thompson@gmail.com>
2026-07-06 19:08:51 +1000

    Auto-backup: 2026-07-06 19:08:51

--- home.nix
+++ home.nix
@@ -1,38 +1,34 @@
 { pkgs, inputs, ... }:
-{
-  home.stateVersion = "26.05";
-  home.username = "willisk";
-  home.homeDirectory = "/home/willisk";
-
-  programs.home-manager.enable = true;
-
-  programs.helix = {
-    enable = true;
-    defaultEditor = true;
-    settings = {
-      theme = "onedark";
-      editor.cursor-shape.insert = "bar";
+  {
+    home.stateVersion = "26.05";
+    home.username = "willisk";
+    home.homeDirectory = "/home/willisk";
+    programs.home-manager.enable = true;
+    programs.helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "onedark";
+        editor.cursor-shape.insert = "bar";
+      };
     };
-  };
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "JetBrainsMono Nerd Font";
-      font_size = 11;
-      confirm_os_window_close = 0;
+    programs.kitty = {
+      enable = true;
+      settings = {
+        font_family = "JetBrainsMono Nerd Font";
+        font_size = 11;
+        confirm_os_window_close = 0;
+      };
     };
-  };
-
-  home.packages = with pkgs; [
-    wlr-which-key
-    fuzzel
-    playerctl
-    brightnessctl
-    wireplumber
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
-
-  xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
-  xdg.configFile."niri/noctalia.kdl".source = ./noctalia.kdl;
-}
+    home.packages = with pkgs; [
+      wlr-which-key
+      fuzzel
+      playerctl
+      brightnessctl
+      wireplumber
+      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+    xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
+    xdg.configFile."niri/noctalia.kdl".source = ./noctalia.kdl;
+    xdg.configFile."wlr-which-key/modal.yaml".source = ./wlr-which-key-modal.yaml;
+  }
