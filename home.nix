5ecebad34bb12099722090c2286d237bb5f29987
Will Thompson <will.lachlan.thompson@gmail.com>
2026-07-07 10:49:54 +1000

    Auto-backup: 2026-07-07 10:49:54

--- home.nix
+++ home.nix
@@ -1,40 +1,46 @@
 { pkgs, inputs, ... }:
-  {
-    home.stateVersion = "26.05";
-    home.username = "willisk";
-    home.homeDirectory = "/home/willisk";
-    programs.home-manager.enable = true;
+let
+  niriCmd = pkgs.writeShellScriptBin "niri-cmd" ''
+    export XDG_RUNTIME_DIR=/run/user/1000
+    export NIRI_SOCKET=$(ls /run/user/1000/niri.wayland-*.sock 2>/dev/null | head -1)
+    ${pkgs.niri}/bin/niri msg action "$@"
+  '';
+in
+{
+  home.stateVersion = "26.05";
+  home.username = "willisk";
+  home.homeDirectory = "/home/willisk";
+  programs.home-manager.enable = true;
 
-    home.file."bin/niri-cmd.sh" = {
-      source = ./niri-cmd.sh;
-      executable = true;
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "onedark";
+      editor.cursor-shape.insert = "bar";
     };
-    
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        theme = "onedark";
-        editor.cursor-shape.insert = "bar";
-      };
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "JetBrainsMono Nerd Font";
+      font_size = 11;
+      confirm_os_window_close = 0;
     };
-    programs.kitty = {
-      enable = true;
-      settings = {
-        font_family = "JetBrainsMono Nerd Font";
-        font_size = 11;
-        confirm_os_window_close = 0;
-      };
-    };
-    home.packages = with pkgs; [
-      wlr-which-key
-      fuzzel
-      playerctl
-      brightnessctl
-      wireplumber
-      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
-    xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
-    xdg.configFile."niri/noctalia.kdl".source = ./noctalia.kdl;
-    xdg.configFile."wlr-which-key/modal.yaml".source = ./wlr-which-key-modal.yaml;
-  }
+  };
+
+  home.packages = with pkgs; [
+    niriCmd
+    wlr-which-key
+    fuzzel
+    playerctl
+    brightnessctl
+    wireplumber
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
+
+  xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
+  xdg.configFile."niri/noctalia.kdl".source = ./noctalia.kdl;
+  xdg.configFile."wlr-which-key/modal.yaml".source = ./wlr-which-key-modal.yaml;
+}
