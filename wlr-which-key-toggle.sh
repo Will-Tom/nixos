cf1c1ebd5f23ac969f30ffde9ef15bc7abc7e690
Will Thompson <will.lachlan.thompson@gmail.com>
2026-07-07 23:18:30 +1000

    Auto-backup: 2026-07-07 23:18:30

--- wlr-which-key-toggle.sh
+++ wlr-which-key-toggle.sh
@@ -2,5 +2,10 @@
 if pgrep -x wlr-which-key > /dev/null; then
     pkill -x wlr-which-key
 else
-    wlr-which-key /home/willisk/.config/wlr-which-key/modal.yaml
+    systemd-run --user --no-block \
+      -E WAYLAND_DISPLAY="$WAYLAND_DISPLAY" \
+      -E XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
+      -E HOME="$HOME" \
+      -E PATH="$PATH" \
+      wlr-which-key "$HOME/.config/wlr-which-key/modal.yaml"
 fi
