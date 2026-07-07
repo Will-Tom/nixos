12aa7118df83a7f3a708eff10a13363f8396e905
Will Thompson <will.lachlan.thompson@gmail.com>
2026-07-07 23:07:17 +1000

    Auto-backup: 2026-07-07 23:07:17

--- wlr-which-key-home.sh
+++ wlr-which-key-home.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 trap '' TERM
 pkill -x wlr-which-key 2>/dev/null
-sleep 0.15
-exec wlr-which-key "$HOME/.config/wlr-which-key/modal.yaml"
+sleep 0.3
+systemd-run --user --no-block \
+  -E WAYLAND_DISPLAY="$WAYLAND_DISPLAY" \
+  -E XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
+  -E HOME="$HOME" \
+  -E PATH="$PATH" \
+  wlr-which-key "$HOME/.config/wlr-which-key/modal.yaml"
