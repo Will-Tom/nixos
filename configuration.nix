aae9e3d1ee54adc524162b345a0b17c585eea4a7
Will Thompson <willthompson696@gmail.com>
2026-07-12 06:19:29 +1000

    Auto-backup: 2026-07-12 06:19:29

--- configuration.nix
+++ configuration.nix
@@ -5,11 +5,16 @@
       inputs.helium-flake.nixosModules.default
     ];
 
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
+  
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   fileSystems."/persist".neededForBoot = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
