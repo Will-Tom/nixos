695659e9a57a99501c50eb6a5450615f317c79d3
Will Thompson <willthompson696@gmail.com>
2026-07-18 14:18:41 +1000

    Auto-backup: 2026-07-18 14:18:41

--- configuration.nix
+++ configuration.nix
@@ -190,7 +190,6 @@
         user = "willisk";
       };
       default_session = {
-        command = "${config.programs.niri.package}/bin/niri-session";
         user = "willisk";
       };
     };
